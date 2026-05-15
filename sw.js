7b8a34ee8d26158ea9eb5bd09cf1368fac49e147
kalbhatgamer-cell <kalbhatgamer@gmail.com>
2026-05-15 22:17:00 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME="jumpers-v1";
+const urls=["./","./index.html","./manifest.json","./icon.png"];
+
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urls)));
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
